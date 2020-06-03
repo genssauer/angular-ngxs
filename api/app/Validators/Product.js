f64f3dfa5c92053d4e8eb85e6b871046304d9cdf
Genesson Sauer <genesson_sauer@hotmail.com>
2020-06-03 17:27:40 -0300

    feat(api): create validators

---
+++ api/app/Validators/Product.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const Antl = use('Antl')
+
+class Product {
+  get validateAll () {
+    return true
+  }
+
+  get rules () {
+    return {
+      title: 'required',
+      description: 'required',
+      price: 'required'
+    }
+  }
+
+  get messages () {
+    return Antl.list('validation')
+  }
+}
+
+module.exports = Product
